321acf4c06941140017c9893e5c0caf334744a41
Jonas Larsson <jonas_larsson1@outlook.com>
2024-02-10 14:07:17 +0100

    added salt and getHash function

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose"
 import crypto from "crypto"
 
-
+// borde alla crypto saker ligga i en egen fil?
+const salt = "örtSalt";
+const getHash = (password) => {
+  let hash = crypto.pbkdf2Sync(
+    password, salt, 1000, 64, 'sha512'
+    ).toString('hex')
+  return hash 
+}
+// ---------------------------------
 
 const userSchema = mongoose.Schema({
   name: String,
@@ -9,7 +17,7 @@ const userSchema = mongoose.Schema({
   password_hash: String,
   club_id:  [{type: Schema.Types.ObjectId, ref: "clubs"}]
 })
-// [{ type: mongoose.Schema.Types.ObjectId, ref: "books" }]
+
 const userModel = mongoose.model('users', userSchema)
 
 export default function user(server) {
