b8a36f137e00ea035b46473fd56c748eb33c9f72
Jonas Larsson <jonas_larsson1@outlook.com>
2024-02-10 14:24:31 +0100

    created post route in users api

--- server/api-register.js
+++ server/api-register.js
@@ -1,7 +1,9 @@
 import club from "./api/club.js";
 import login from "./api/login.js";
+import user from "./api/users.js";
 
 export default function (server) {
   club(server)
   login(server)
+  user(server)
 }
--- server/api/users.js
+++ server/api/users.js
@@ -11,12 +11,14 @@ const getHash = (password) => {
 }
 // ---------------------------------
 
+// borde alla schemas ligga i egna filer i en 'models' mapp?
 const userSchema = mongoose.Schema({
   name: String,
   email: String,
   password_hash: String,
-  club_id:  [{type: Schema.Types.ObjectId, ref: "clubs"}]
+  club_id:  [{type: mongoose.Schema.Types.ObjectId, ref: "clubs"}]
 })
+// ---------------------------------
 
 const userModel = mongoose.model('users', userSchema)
 
@@ -24,6 +26,24 @@ export default function user(server) {
 
   server.get('/api/user', async (req, res) => {
     res.json(await userModel.find())
+  })
+
+  server.post('/api/user', async (req, res) => {
+    try {
+      const newUser = new userModel({
+        name: req.body.name,
+        email: req.body.email,
+        password_hash: getHash(req.body.password),
+        club_id: req.body.club_id
+      })
+      const savedUsed = await newUser.save()
+      res.status(201).json(savedUsed)
+    } catch (error) {
+      res.status(500).json({
+        message: "Error creating new user",
+        error: error 
+      })
+    }
   })
 
 }
