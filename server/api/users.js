6371f5b7ea99d1f529cd4157fb238340f71f3f21
Jonas Larsson <jonas_larsson1@outlook.com>
2024-02-10 14:35:06 +0100

    name, email and password required for new users

--- server/api/users.js
+++ server/api/users.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 import crypto from "crypto"
 
 // borde alla crypto saker ligga i en egen fil?
-const salt = "örtSalt";
+const salt = "örtSalt"
 const getHash = (password) => {
   let hash = crypto.pbkdf2Sync(
     password, salt, 1000, 64, 'sha512'
@@ -30,14 +30,22 @@ export default function user(server) {
 
   server.post('/api/user', async (req, res) => {
     try {
+      if (!req.body.name || !req.body.email || !req.body.password) {
+        return res.status(400).json({
+          message: "Missing required fields"
+        })
+      }
+
       const newUser = new userModel({
         name: req.body.name,
         email: req.body.email,
         password_hash: getHash(req.body.password),
         club_id: req.body.club_id
       })
-      const savedUsed = await newUser.save()
-      res.status(201).json(savedUsed)
+
+      const savedUser = await newUser.save()
+      res.status(201).json({ user_id: savedUser._id })
+
     } catch (error) {
       res.status(500).json({
         message: "Error creating new user",
@@ -45,5 +53,4 @@ export default function user(server) {
       })
     }
   })
-
 }
