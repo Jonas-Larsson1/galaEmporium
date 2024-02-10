ee6de0d4ebe2d2fd97071a47daa68325233cec3d
BenjaminSegel <benjamin_segelstrom@hotmail.com>
2024-02-10 20:10:00 +0100

    post and put method added

--- server/api/events.js
+++ server/api/events.js
@@ -15,7 +15,27 @@ const eventModel = mongoose.model('events', eventSchema)
 export default function event(server) {
 
   server.get('/api/event', async (req, res) => {
-    res.json(await eventModel.find())
+    res.json(await eventModel.find().populate("club_id"))
+  })
+
+  server.post('/api/event', async (req, res)=> {
+    try{
+    const newEvent = new eventModel(req.body)
+    const result = newEvent.save()
+    res.json(result)
+    } catch(error){
+        res.json({message: "404: Could not post event", error})
+    }
+    
+  })
+
+  server.put('/api/event/:id', async(req, res)=> {
+    const id = req.params.club_id
+    const updatedEvent = req.body
+    const data = await eventModel.findOneAndUpdate(id, updatedEvent, {
+        new:true
+    })
+    res.json(data)
   })
 
 }
