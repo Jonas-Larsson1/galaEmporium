92a1463b9e183b156851ea571b2c7cd3a525d5d7
BenjaminSegel <benjamin_segelstrom@hotmail.com>
2024-02-12 15:18:14 +0100

    create event form added

--- client/pages/event.js
+++ client/pages/event.js
@@ -10,8 +10,9 @@ export default async function event(){
         console.log(data)
     html+= `
     <h1>${data.name} </h1>
-    <h2>${data.description} </h2>
-    <h2>${data.club_id.name}</h2> 
+    <img src="${data.img}">
+    <h2>${data.club_id.name}</h2>
+    <h2>${data.description} </h2> 
     <ol>    
         <li>Cost: ${data.cost}</li> 
         <li>Date: ${data.date}</li> 
@@ -20,12 +21,20 @@ export default async function event(){
     return `<article>${html}</article>`
 }
 
-/* export default function createEvent(){
+ export function createEvent(){
     return `
         <form> 
         <h1> Create Event </h1> 
         <input type="text" name="name" placeholder="Event name">
-        <input type="tetxt" name="description" placeholder="Event description">
+        <input type="text" name="image" placeholder="Insert image URL">
+        <label for="club">Choose club: </label>
+        <select name="club">
+            <option></option>
+            <option value="SpaceX">SpaceX</option>
+        </select>  
+        <input type="text" name="description" placeholder="Event description">
+        <input type="number" name="cost" placeholder="Ticket cost">
+        <input type="date" name="date" placeholder="Date for event"> 
         </form>
     `
-} */
+} 
--- client/router.js
+++ client/router.js
@@ -2,13 +2,14 @@ import home from "./pages/home.js";
 import club from "./pages/club.js";
 import login from "./pages/login.js";
 import event from "./pages/event.js";
+import { createEvent } from "./pages/event.js";
 
 async function route() {
   //console.log(location)
 
   switch (location.hash.replace('#', '')) {
     case "":
-      $('main').html(await event())
+      $('main').html(createEvent())
       break;
     case "club":
       $('main').html(club())
