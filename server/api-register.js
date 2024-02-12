06cab2bb4e953aedb1fbe916cf1e6802d58e59dc
Jonas Larsson <jonas_larsson1@outlook.com>
2024-02-12 14:08:11 +0100

    fixed api register

--- server/api-register.js
+++ server/api-register.js
@@ -1,12 +1,9 @@
 import club from "./api/club.js";
-import login from "./api/login.js";
-import event from "./api/events.js";
 import user from "./api/users.js";
 import event from "./api/events.js";
 
 export default function (server) {
   club(server)
-  login(server)
   user(server)
   event(server)
 }
