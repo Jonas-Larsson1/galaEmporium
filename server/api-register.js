65cdbced1051adfa0b6611a2ef0da302dce60f69
djaevil <djaevil1@gmail.com>
2024-02-12 14:23:45 +0100

    get all bookings & make a booking

--- server/api-register.js
+++ server/api-register.js
@@ -1,7 +1,9 @@
 import club from "./api/club.js";
 import user from "./api/users.js";
+import booking from "./api/bookings.js";
 
 export default function (server) {
   club(server)
   user(server)
+  booking(server)
 }
--- server/api/bookings.js
+++ server/api/bookings.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
 const bookingSchema = mongoose.Schema({
-  event_id: {type: Schema.Types.ObjectId, ref: "events"},
-  user_id: {type: Schema.Types.ObjectId, ref: "users"},
+  event_id: {type: mongoose.Schema.Types.ObjectId, ref: "events"},
+  user_id: {type: mongoose.Schema.Types.ObjectId, ref: "users"},
   seats_reserved: Number, 
   paid: Boolean  
 })
@@ -11,8 +11,31 @@ const bookingModel = mongoose.model('bookings', bookingSchema)
 
 export default function booking(server) {
 
-  server.get('/api/booking', async (req, res) => {
-    res.json(await bookingModel.find())
+  server.get('/api/bookings', async (req, res) => {
+    try {
+      res.json(await bookingModel.find())
+    } catch (error) {
+      res.status(500).json({ message: "There was a problem fetching bookings from the server." })
+    }
+  })
+
+  server.post('/api/bookings', async (req, res) => {
+    try {
+      if(req.session.user) {
+        const newBooking = new bookingModel({
+          event_id: req.body.event_id,
+          user_id: req.session.user,
+          seats_reserved: req.body.seats_reserved,
+          paid: req.body.paid
+        })
+        const savedBooking = await newBooking.save();
+        res.status(201).json(savedBooking);
+      } else {
+        res.status(403).json({ messgae: "You need to be logged in!" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "There was a problem adding the booking." });
+    }
   })
 
 }
