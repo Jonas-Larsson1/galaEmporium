629c339ebc841e0a5778298940d47f1ebd8d27cd
Heli Hård Källoff <vierge.hard@gmail.com>
2024-02-12 15:31:59 +0100

    Initial setup cart

---
+++ client/components/cart.js
@@ -0,0 +1,4 @@
+// access elements in html
+const addToCartBtn = document.querySelector("#add-to-cart-btn");
+const emptyCartBtn = document.querySelector("#empty-cart-btn");
+const cartSummary = document.querySelector("#cart-summary");
