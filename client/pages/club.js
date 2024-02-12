4a163205711671ba5f00c40e4a88b6b6101ffb53
djaevil <djaevil1@gmail.com>
2024-02-12 15:19:03 +0100

    fix in api-register & start coding template

--- client/pages/club.js
+++ client/pages/club.js
@@ -1,10 +1,13 @@
-export default function club() {
+export default async function club() {
 
-    let text = "blissful"
+  const response = await fetch('/api/clubs/:id')
+  const result = await response.json()
+
+  console.log(result);
   
     return `
     <article id="home">
-      <h1>Welcome to my ${text} page</h1>
+      <h1>Welcome to ${data.name}</h1>
       <p>OBS! Glöm inte CSS.</p>
     </article>
     `
