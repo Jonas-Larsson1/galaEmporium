3bd0e1a96e9fb7d24cc0792356f8ddb2512866ff
Heli Hård Källoff <vierge.hard@gmail.com>
2024-02-14 10:30:16 +0100

    html elements module added

---
+++ client/components/html-elements.js
@@ -0,0 +1,5 @@
+// access elements in html
+export const addToCartBtn = document.querySelector("#add-to-cart-btn");
+export const emptyCartBtn = document.querySelector("#empty-cart-btn");
+export const cartSummary = document.querySelector("#cart-summary");
+export const submitBookingBtn = document.querySelector("#submit-booking");
