6421b4b3b8b805eab0117923913c8f816eb6529f
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-02-16 11:04:44 +0100

    events history created

--- client/components/mypage.js
+++ client/components/mypage.js
@@ -7,25 +7,38 @@ export default function mypage() {
        $.get(`/api/booking/user/${userId}`, function (data) {
         if (userId) {
           console.log("User ID:", userId);
-          data.forEach(booking => {
-            const buttonClass = booking.paid ? "paid" : "pay";
-            const disabledAttribute = booking.paid ? "disabled" : "";
+          console.log(data.length)
+          if (data.length == 0) {
+            content += "<h1>Inga evenemang är bokade</h1>"
+          } else {
+            data.forEach(booking => {
+              
+              const buttonClass = booking.paid ? "paid" : "pay";
+              const disabledAttribute = booking.paid ? "disabled" : "";
 
-            content += `
-        <div class="container">
-          <p>${booking.event_id.description}</p>
-          <p>${booking.seats_reserved}</p>
-          <button class="${buttonClass}" ${disabledAttribute}>${booking.paid ? "OK" : "BETALA"}</button>
-        </div> 
-      `
+              const eventDate = new Date(booking.event_id.date);
+              const formattedDate = eventDate.toLocaleDateString(); 
+              const formattedTime = eventDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
+
+              content += `
+          <div class="container">
+            <img class='container_img' src=${booking.event_id.img}/>
+            <p>${booking.event_id.name}</p>
+            <p>${formattedDate} ${formattedTime}</p>
+            <p>${booking.event_id.cost} SEK</p>
+            <p>${booking.seats_reserved}</p>
+            <button class="${buttonClass} container-button" ${disabledAttribute}>${booking.paid ? "OK" : `${booking.event_id.cost * booking.seats_reserved} SEK`}</button>
+          </div> 
+        `
+        
+      })};
       
-    });
-    
-    $('#container-xl').html(content);
+      $('#container-xl').html(content);
        }    else {
-        console.log('User ID är inte tillgänglig')
+        console.log('User ID är inte tillgänglig');
+        ;
        }
-       
+      //  $('#container-xl').html(content);
    })
 
    
