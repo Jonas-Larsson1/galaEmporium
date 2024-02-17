74aac667eecaba626484da7d5eef4645a374db96
Jonas Larsson <jonas_larsson1@outlook.com>
2024-02-17 15:08:38 +0100

    form for editing events

--- client/pages/editEventPage.js
+++ client/pages/editEventPage.js
@@ -1,13 +1,49 @@
 export default async function editEventPage(eventId){
-    // const response = await fetch(`/api/event/${eventId}`)
-    // const event = await response.json()
     const event = await (await fetch(`/api/event/${eventId}`)).json()
-    
-    console.log(event) 
 
     return `
-    <input type="text" name="name" value="${event.name}">
-    
-    `
-    
+    <form id="editEventForm">
+        <label for="name">Change name</label><br>
+        <input type="text" id="name" name="name" value="${event.name}"><br>
+        
+        <label for="description">Change description</label><br>
+        <input type="text" id="description" name="description" value="${event.description}"><br>
+        
+        <label for="cost">Change cost</label><br>
+        <input type="number" id="cost" name="cost" value="${event.cost}"><br>
+
+        <label for="date">Change date</label><br>
+        <input type="datetime-local" id="date" name="date" value="${event.date}"><br>
+
+        <label for="img">Change image</label><br>
+        <input type="text" id="img" name="img" value="${event.img}"><br>
+        
+        <input type="submit" value="Submit" onclick="updateEvent('${eventId}'); return false;">
+    </form>
+    `  
 }
+
+ async function updateEvent(eventId){
+
+    const updatedData = {
+        name: $('[name="name"]').val(),
+        description: $('[name="description"]').val(),
+        cost: $('[name="cost"]').val(),
+        date: $('[name="date"]').val(),
+        img: $('[name="img"]').val(),
+    }
+
+    const thisEvent = await fetch(`/api/event/${eventId}`, {
+        method: "put" ,
+          headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedData)
+    })
+    const res = await thisEvent.json()
+    console.log(res)
+} 
+    
+
+
+window.updateEvent = updateEvent
