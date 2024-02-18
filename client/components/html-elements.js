b70722d43c4606b4b41859d014a43a366933e1de
Jonas Larsson <jonas_larsson1@outlook.com>
2024-02-18 14:29:49 +0100

    clean up branch

--- client/components/html-elements.js
+++
@@ -1,7 +0,0 @@
-
-
-// access elements in html
-// export const addToCartBtn = document.querySelector("#add-to-cart-btn");
-//  export const emptyCartBtn = document.querySelector("#empty-cart-btn");
-//  export const cartSummary = document.querySelector("#cart-summary");
-//  export const submitBookingBtn = document.querySelector("#submit-booking");
