c079e5b79d2d8da9d4d4f5ff48fd892f052cdf3d
Jonas Larsson <jonas_larsson1@outlook.com>
2024-02-19 14:56:21 +0100

    cleanup in cart page

--- client/pages/cart.js
+++ client/pages/cart.js
@@ -1,42 +1,41 @@
 export default function cart(){
     
     const cartContents = JSON.parse(sessionStorage.getItem('cartContents'));
-    console.log(cartContents)
 
-    const eventIdCount = {};
-    cartContents.forEach(item => {
-        eventIdCount[item.event_id] = eventIdCount[item.event_id] || { count: 0, rendered: false };
-        eventIdCount[item.event_id].count++;
-    });
-
-    let html = '';
-
+    let cartItemsHtml = ''
     cartContents.forEach(item => {
         const eventDate = new Date(item.date);
         const formattedDate = eventDate.toLocaleDateString(); 
         const formattedTime = eventDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
         
-            html += `
+        cartItemsHtml += `
             <div class="main-container">
-            <p>${item.title}</p>
-            <p>${formattedDate} ${formattedTime}</p>
+                <p>${item.title}</p>
+                <p>${formattedDate} ${formattedTime}</p>
             <div>
                 <button class="count-btn" data-event-id="${item.event_id}">${item.amount}</button>
                 <button class="increase-btn" data-event-id="${item.event_id}">+</button>
                 <button class="decrease-btn" data-event-id="${item.event_id}">-</button>
             </div> 
-            <div class="price" data-price-id="${item.event_id}">${item.totalPrice}</div>
-            <div class="price" data-singlePrice-id="${item.event_id}">${item.price}</div>
+                <div class="price" data-price-id="${item.event_id}">${item.totalPrice}</div>
+                <div class="price" data-singlePrice-id="${item.event_id}">${item.price}</div>
             </div>
-            `;
+
+        `;
         
     });
-    const cartDiv = document.createElement('div');
-    cartDiv.classList.add('cart-container');
+
+    let html = `
+    <div class="cart-container">
+        ${cartItemsHtml}
+    </div>
+    `
+    // const cartDiv = document.createElement('div');
+    // cartDiv.classList.add('cart-container');
     
-    document.body.appendChild(cartDiv);
-    $('.cart-container').html(html);
-    $('main').html(cartDiv);
+    // document.body.appendChild(cartDiv);
+    // $('.cart-container').html(html);
+    // $('main').html(cartDiv);
 
     $('.increase-btn').click(function() {
         const eventId = $(this).data('event-id');
@@ -70,10 +69,10 @@ export default function cart(){
         
     }
     
-    // return html;
     function updateCartContents() {
         sessionStorage.setItem('cartContents', JSON.stringify(cartContents)); // Update sessionStorage with the modified cartContents
     }
-
+    
+    return html;
 }
 
